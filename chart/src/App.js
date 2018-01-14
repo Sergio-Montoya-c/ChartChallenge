d268095d3364833f057b91a51c4ee5e7e74a057d
Sergio Montoya <checo@Sergios-iMac.local>
2018-01-14 11:23:25 -0600

    Chart finished

---
+++ chart/src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './App.css';
+import RC2 from 'react-chartjs2';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import Campaign from './Campaign.js';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      campaignMonths: Campaign.months,
+    }
+  }
+
+  render() {    
+    const {campaignMonths } = this.state;
+    const monthsArray = Array.from(Array(campaignMonths).keys());
+
+    let competitorsDataset = Campaign.competitors.reverse().map((competitor, index) => {
+      const campaignTotal = ((competitor.medium) + (competitor.strong) + (competitor.resource)) / Campaign.months;
+      return {
+        label: competitor.url,
+        backgroundColor: '#dadada',
+        borderColor: '#ccc',
+        borderWidth: 2,
+        data: monthsArray.map(month => { return campaignTotal * (month + 1) })
+      }
+    });
+
+    const ownCampaignTotal = ((Campaign.suggestMedium) + (Campaign.suggestStrong) + (Campaign.suggestResource)) / Campaign.months;
+    const ownDataset = {
+      label: Campaign.campaignUrl,
+      backgroundColor: '#478FFF',
+      borderColor: '#214275',
+      borderWidth: 2,
+      data: monthsArray.map(month => { return ownCampaignTotal * (month + 1) })
+    }
+
+    competitorsDataset = [...competitorsDataset, ownDataset];
+
+    var barChartData = {
+      labels: monthsArray,
+      datasets: competitorsDataset,
+    };
+
+    const options = {
+      responsive: true,
+      scales: {
+        yAxes: [{
+          stacked: true,
+        }]
+      },
+      legend: {
+          position: 'top',
+      },
+      tooltips: {
+        mode: 'index',
+        position: 'nearest'
+      },
+      title: {
+          display: true,
+          text: 'AMV Competitors Chart'
+      }
+    }
+
+    return (
+      <div className="App" style={{display: 'flex'}}>
+        <div style={{padding: '25px', width: '25%'}}>
+          <label>Months: {campaignMonths}</label>
+          <Slider min={3} max={24} onChange={ value => { this.setState({campaignMonths: value }) } } defaultValue={campaignMonths}/>
+        </div>
+        <div style={{padding: '25px', width: '75%'}}>
+          <RC2 data={barChartData} options={options} type='line' />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ chart/src/Campaign.js
@@ -0,0 +1,43 @@
+let Campaign = {}
+
+export default Campaign = {
+  "competitors": [
+    {
+      "url" : "http://cuervo.com/",
+      "resource" : 59,
+      "strong" : 102,
+      "medium" : 71
+    },
+    {
+      "url" : "http://cuervo.com/products/",
+      "resource" : 3,
+      "strong" : 1,
+      "medium" : 4
+    },
+    {
+      "url" : "https://www.reservebar.com/tequila/jose-cuervo",
+      "resource" : 1,
+      "strong" : 0,
+      "medium" : 2
+    },
+    {
+      "url" : "https://www.hangoverprices.com/jose-cuervo-prices/",
+      "resource" : 0,
+      "strong" : 1,
+      "medium" : 0
+    },
+    {
+      "url" : "http://www.totalwine.com/spirits/brand/jose-cuervo",
+      "resource" : 0,
+      "strong" : 0,
+      "medium" : 0
+    }
+  ],
+  "beatBy":1.1,
+  "months":12,
+  "target":1,
+  "campaignUrl":"cuervogold.com",
+  "suggestMedium":79,
+  "suggestStrong":113,
+  "suggestResource":65
+}
